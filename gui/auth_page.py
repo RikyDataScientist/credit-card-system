fca39d790ad01b9b64f944c9129967d55e0af8a9
Faris Riky Pratama <farisriky01@gmail.com>
2025-11-19 11:15:58 +0700

    auth_page adjusment and linked auth and user gui with controller.

--- gui/auth_page.py
+++ gui/auth_page.py
@@ -1,10 +1,13 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QStackedWidget, QFrame, QVBoxLayout, QPushButton, QLineEdit, QLabel, QSpacerItem, QSizePolicy
+from PyQt6.QtWidgets import QApplication, QWidget, QStackedWidget, QFrame, QVBoxLayout, QPushButton, QLineEdit, QLabel, QMessageBox
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap, QFont
 
 class Login(QWidget):
-    def __init__(self, stack):
+    def __init__(self, stack, controller):
         super().__init__()
+        self.stack = stack
+        self.controller = controller
+
         title = QLabel('Login in to HimBank')
         title.setAlignment(Qt.AlignmentFlag.AlignCenter)
         title.setObjectName('title')
@@ -25,12 +28,12 @@ class Login(QWidget):
         )
         link.setTextFormat(Qt.TextFormat.RichText)
         link.setOpenExternalLinks(False)
-        link.linkActivated.connect(lambda: stack.setCurrentIndex(1))
+        link.linkActivated.connect(lambda: self.stack.setCurrentIndex(1))
         link.setObjectName('subtitle')
         link.setAlignment(Qt.AlignmentFlag.AlignRight)
 
         self.button = QPushButton("Login")
-        self.button.clicked.connect(lambda: stack.setCurrentIndex(2))
+        self.button.clicked.connect(self.execute)
         self.button.setObjectName('button')
 
         frame = QFrame()
@@ -50,97 +53,100 @@ class Login(QWidget):
         mainLayout.addWidget(frame)
         mainLayout.addStretch()
 
-class SignupPage(QWidget):
-    def __init__(self):
+    def execute(self):
+        username = self.username.text()
+        password = self.password.text()
+
+        try:
+            message = self.controller.login(username, password)
+            print(message)
+            self.stack.setCurrentIndex(2)
+        except Exception as msg:
+            self.show_error(str(msg))
+
+    def show_error(self, msg):
+        popup = QMessageBox()
+        popup.setIcon(QMessageBox.Icon.Critical)
+        popup.setText(msg)
+        popup.exec()
+
+
+class Register(QWidget):
+    def __init__(self, stack, controller):
         super().__init__()
-        label = QLabel("This is Signup Page", alignment=Qt.AlignmentFlag.AlignCenter)
-        label.setStyleSheet("font-size:20px; color:white;")
+        self.stack = stack
+        self.controller = controller
+
+        title = QLabel('Register Account')
+        title.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        title.setObjectName('title')
+
+        self.chn = QLineEdit()
+        self.chn.setPlaceholderText('Card Holder Name')
+        self.chn.setObjectName('inputtext')
+
+        self.username = QLineEdit()
+        self.username.setPlaceholderText('Username')
+        self.username.setObjectName('inputtext')
+
+        self.password = QLineEdit()
+        self.password.setPlaceholderText('Password')
+        self.password.setEchoMode(QLineEdit.EchoMode.Password)
+        self.password.setObjectName('inputtext')
+
+        link = QLabel()
+        link.setText(
+            "<span style='color:white;'>Do you have an account?</span> "
+            "<a href='signup' style='color:#4da3ff;'>Click here</a>"
+        )
+        link.setTextFormat(Qt.TextFormat.RichText)
+        link.setOpenExternalLinks(False)
+        link.linkActivated.connect(lambda: self.stack.setCurrentIndex(0))
+        link.setObjectName('subtitle')
+        link.setAlignment(Qt.AlignmentFlag.AlignRight)
+
+        self.button = QPushButton("Register")
+        self.button.clicked.connect(self.execute)
+        self.button.setObjectName('button')
+
+        frame = QFrame()
+        frame.setObjectName('card')
+        frameLayout = QVBoxLayout(frame)
+
+        frameLayout.addWidget(title)
+        frameLayout.addSpacing(30)
+        frameLayout.addWidget(self.chn)
+        frameLayout.addWidget(self.username)
+        frameLayout.addWidget(self.password)
+        frameLayout.addWidget(link)
+        frameLayout.addSpacing(20)
+        frameLayout.addWidget(self.button)
+
+        mainLayout = QVBoxLayout(self)
+        mainLayout.addStretch()
+        mainLayout.addWidget(frame)
+        mainLayout.addStretch()
+
+    def execute(self):
+        cardholder_name = self.chn.text()
+        username = self.username.text()
+        password = self.password.text()
+
+        try:
+            message = self.controller.register(cardholder_name, username, password)
+            print(message)
+            self.stack.setCurrentIndex(0)
+        except Exception as msg:
+            self.show_error(str(msg))
+
+    def show_error(self, msg):
+        popup = QMessageBox()
+        popup.setIcon(QMessageBox.Icon.Critical)
+        popup.setText(msg)
+        popup.exec()
 
 class MainPage(QWidget):
-    def __init__(self):
+    def __init__(self, stack):
         super().__init__()
         label = QLabel("This is Main Dashboard", alignment=Qt.AlignmentFlag.AlignCenter)
         label.setStyleSheet("font-size:20px; color:white;")
-
-
-# --- MAIN APP TEST ---
-app = QApplication([])
-
-# Stacked widget
-stack = QStackedWidget()
-
-# Pages
-login = Login(stack)
-signup = SignupPage()
-main = MainPage()
-
-# Tambahkan ke stack
-stack.addWidget(login)   # index 0
-stack.addWidget(signup)  # index 1
-stack.addWidget(main)    # index 2
-
-stack.setCurrentIndex(0)
-stack.setFixedSize(420, 520)
-
-# --- CSS ---
-app.setStyleSheet("""
-        QWidget {
-            background-color: #0d1117;
-            color: #c9d1d9;
-            font-family: 'Segoe UI';
-        }
-
-        #card {
-            background-color: #161b22;
-            border: 1px solid #1f2937;
-            border-radius: 18px;
-            padding: 35px;
-        }
-
-        #title {
-            font-size: 28px;
-            font-weight: bold;
-            color: #58a6ff;
-            margin-bottom: 5px;
-        }
-
-        #subtitle {
-            font-size: 14px;
-            color: #8b949e;
-            margin-bottom: 20px;
-        }
-
-        #inputtext {
-            background-color: #0d1117;
-            border: 2px solid #30363d;
-            padding: 12px;
-            border-radius: 10px;
-            font-size: 15px;
-            margin-bottom: 18px;
-            color: #c9d1d9;
-        }
-
-        #inputtext:focus {
-            border: 2px solid #58a6ff;
-            background-color: #0c162d;
-        }
-
-        #button {
-            background-color: #238636;
-            color: white;
-            padding: 12px;
-            border-radius: 10px;
-            font-size: 16px;
-        }
-
-        #button:hover {
-            background-color: #2ea043;
-        }
-
-        #button:pressed {
-            background-color: #196c2e;
-        }
-        """)
-
-stack.show()
-app.exec()
